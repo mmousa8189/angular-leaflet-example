5e206fa83398752883d0e2834855ae4d5aa7f89d
Mohamed_Mousa <Mohamed_Mousa@efinance.com.eg>
2022-01-11 10:06:03 +0200

    init add usa map with stats layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MarkerService } from './marker.service';
+import { PopupService } from './popup.service';
+import { ShapeService } from './shape.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MarkerService,PopupService,ShapeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
