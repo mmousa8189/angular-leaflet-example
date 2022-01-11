af709f8be0d96dee42aabe50f855e0752e3c5fad
Mohamed Mousa <mohamedamousa00@gmail.com>
2022-01-11 12:48:14 +0200

    add egypt shape

--- src/app/shape.service.ts
+++ src/app/shape.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShapeService {
-
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {}
 
   getStateShapes() {
     return this.http.get('/assets/data/gz_2010_us_040_00_5m.json');
+  }
+
+  getEgyptShape() {
+    return this.http.get(
+      '/assets/data/egy_admbnda_adm0_capmas_itos_20170421.geojson'
+    );
   }
 }
