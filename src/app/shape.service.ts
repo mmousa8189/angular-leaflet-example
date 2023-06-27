623eb45556d79e01a9cf5608292ad898a8da3678
mmousa8189 <mohamedamousa00@gmail.com>
2023-06-27 23:00:27 +0300

    change /angular-leaflet-example/assets

--- src/app/shape.service.ts
+++ src/app/shape.service.ts
@@ -7,23 +7,25 @@ import { HttpClient } from '@angular/common/http';
 export class ShapeService {
   // egy_admbnda_adm0_capmas_itos_20170421.geojson => for egypt boundaries
   private egyptBoundaries =
-    '/assets/data/egy_admbnda_adm0_capmas_itos_20170421.geojson';
+    '/angular-leaflet-example/assets/data/egy_admbnda_adm0_capmas_itos_20170421.geojson';
   // egy_admbnda_adm1_capmas_20170421.geojson => for egypt states boundaries
   private egyptStatesBoundaries =
-    '/assets/data/egy_admbnda_adm1_capmas_20170421.geojson';
+    '/angular-leaflet-example/assets/data/egy_admbnda_adm1_capmas_20170421.geojson';
   // egy_admbnda_adm1_capmas_20170421.geojson => for egypt states maraks boundaries
   private egyptMarakzBoundaries =
-    '/assets/data/egy_admbnda_adm2_capmas_20170421.geojson';
+    '/angular-leaflet-example/assets/data/egy_admbnda_adm2_capmas_20170421.geojson';
   // egy_admbnda_adm1_capmas_20170421.geojson => for egypt states Cities boundaries
   private egyptCitiesBoundaries =
-    '/assets/data/egy_admbnda_adm3_capmas_20170421.geojson';
+    '/angular-leaflet-example/assets/data/egy_admbnda_adm3_capmas_20170421.geojson';
   // egy_admbnda_adm1_capmas_20170421.geojson => for egypt Places Pins
   private egyptPlacesPins =
-    '/assets/data/egy_admbndp_admall_capmas_itos_20170421.geojson';
+    '/angular-leaflet-example/assets/data/egy_admbndp_admall_capmas_itos_20170421.geojson';
   constructor(private http: HttpClient) {}
 
   getStateShapes() {
-    return this.http.get('/assets/data/gz_2010_us_040_00_5m.json');
+    return this.http.get(
+      '/angular-leaflet-example/assets/data/gz_2010_us_040_00_5m.json'
+    );
   }
 
   getEgyptShape() {
